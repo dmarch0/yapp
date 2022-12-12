660c071680b52a1d82c6f95522b7f3173eb3b430
dmarch0 <daniil2305@yandex.ru>
2022-12-12 10:28:59 +0400

    tech: added new table, minor tables refactoring

--- services/core/internal/db/init_tables.go
+++ services/core/internal/db/init_tables.go
@@ -7,18 +7,19 @@ import (
 
 func InitTables(c context.Context) {
 	connection := GetDbConnection()
-	_, errUsers := connection.Exec(c, `
+
+	usersTable := `
 		CREATE TABLE IF NOT EXISTS users (
 			id SERIAL PRIMARY KEY,
 			email VARCHAR(255) NOT NULL UNIQUE,
 			password VARCHAR(255) NOT NULL
 		);
-	`)
-	if errUsers != nil {
-		log.Panic("Error creating users table", errUsers)
+	`
+	if _, err := connection.Exec(c, usersTable); err != nil {
+		log.Panic("Eror creating users table", err)
 	}
 
-	_, errTokens := connection.Exec(c, `
+	tokensTable := `
 		CREATE TABLE IF NOT EXISTS user_tokens (
 			id SERIAL PRIMARY KEY,
 			token VARCHAR(255) NOT NULL,
@@ -26,9 +27,23 @@ func InitTables(c context.Context) {
 			FOREIGN KEY (user_id)
 				REFERENCES users(id)
 		);
-	`)
+	`
 
-	if errTokens != nil {
-		log.Panic("Error creating tokens table", errTokens)
+	if _, err := connection.Exec(c, tokensTable); err != nil {
+		log.Panic("Eror creating tokens table", err)
+	}
+
+	charactersTable := `
+		CREATE TABLE IF NOT EXISTS characters (
+			id SERIAL PRIMARY KEY,
+			name VARCHAR(255) NOT NULL,
+			user_id INT NOT NULL,
+			FOREIGN KEY (user_id)
+				REFERENCES users(id)
+		);
+	`
+
+	if _, err := connection.Exec(c, charactersTable); err != nil {
+		log.Panic("Eror creating characters table", err)
 	}
 }
