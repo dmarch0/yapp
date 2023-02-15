23db09a7f2f0c35641d6ec738e6217ff25d18116
dmarch0 <daniil2305@yandex.ru>
2023-02-16 00:43:14 +0400

    tech: types

--- services/web-client/src/components/Button/Button.tsx
+++ services/web-client/src/components/Button/Button.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import type { ButtonTypes } from './Button.types';
 interface Props {
     children: React.ReactNode,
-    onClick: React.MouseEventHandler<HTMLButtonElement>
+    onClick: React.MouseEventHandler<HTMLButtonElement>,
+    type: ButtonTypes
 }
 
 const Button: React.FunctionComponent<Props> = ({ children, onClick }) => {
